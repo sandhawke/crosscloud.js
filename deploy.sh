ba19fba9e2ec150eb8d7a546fcdf5fd759bece7d
Sandro Hawke <sandro@hawke.org>
2014-11-27 20:37:54 -0500

    split podlogin doctests into simulated and real, and now they pass reliably

--- deploy.sh
+++ deploy.sh
@@ -38,7 +38,7 @@ done
 echo "copying to servers..."
 # include https://es6-promises.s3.amazonaws.com/es6-promise-2.0.0.min.js" ?
 cat main.js webcircuit.js podlogin.js > crosscloud.js
-rsync -aR crosscloud.js podlogin-test.html podlogin.js README.html example doctest root@www1.crosscloud.org:/sites/crosscloud.org/$VERSION/
+rsync -aR crosscloud.js podlogin.js README.html example doctest test root@www1.crosscloud.org:/sites/crosscloud.org/$VERSION/
 rsync -a podlogin-iframe.html podlogin-iframe.js root@podlogin.org:/sites/podlogin.org/$VERSION/
 ## rsync -a network.html network.js root@fakepods.com:/sites/fakepods.com/_login/$VERSION/
 echo done
