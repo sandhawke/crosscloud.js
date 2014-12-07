be28ca3ec6c2b482c4ac85d12a33c5c1534d8157
Sandro Hawke <sandro@hawke.org>
2014-12-07 12:53:48 -0500

    populate the server with some fake contact information, for demo/testing

---
+++ example/node/avengers.js
@@ -0,0 +1,78 @@
+
+var Promise = require('promise');
+var crosscloud = require('../..');
+
+var person = {
+	nick: 
+	{ 
+		name: "Nick Fury", 
+		selfDescription: "Director",
+		imageURL: "http://fc01.deviantart.net/fs71/f/2010/244/d/4/the_avengers___nick_fury_by_agustin09-d2xr4ck.jpg"
+	},
+	maria:
+	{
+		name: "Maria Hill",
+		selfDescription: "Actually does everything",
+		imageURL: "http://upload.wikimedia.org/wikipedia/commons/2/29/Cobie_Smulders%281%29.jpg"
+	},
+	phil:
+	{
+		name: "Phil Coulson",
+		selfDescription: "Just trying to do my job",
+		imageURL: "http://upload.wikimedia.org/wikipedia/commons/f/f0/Clark_Gregg_by_Gage_Skidmore.jpg"
+	},
+	thor:
+	{
+		name: "Thor",
+		selfDescription: "Genuinely Nice Guy",
+		imageURL: "https://c2.staticflickr.com/4/3726/10412961916_46f51bbf33_b.jpg"},
+	stark:
+	{
+		name: "Tony Stark",
+		selfDescription: "Genius",
+		imageURL: "https://c1.staticflickr.com/9/8403/8709578942_b6d9dd8002_h.jpg"
+	},
+	odin:
+	{
+		name: "Odin",
+		// imageURL: "http://upload.wikimedia.org/wikipedia/commons/f/f8/Odin,_der_G%C3%B6ttervater.jpg"
+		imageURL: "http://upload.wikimedia.org/wikipedia/commons/9/9f/Odin_%28Manual_of_Mythology%29.jpg"
+	}
+};
+
+var contacts = [
+	{from:person.nick, to:person.phil},
+	{from:person.nick, to:person.maria},
+	{from:person.nick, to:person.thor},
+	{from:person.nick, to:person.stark},
+	{from:person.thor, to:person.odin}
+];
+
+var connection = {};
+
+base = "http://localhost:8080/pod/avengers_";
+base = "http://localhost:8080/pod/";
+for (var key in person) {
+	person[key]._id = base+key+"/";
+}
+
+var promises = [];
+for (var key in person) {
+	var profile = person[key];
+	var pod = crosscloud.connect({podURL:profile._id});
+	connection[profile._id] = pod;
+	pod.push(profile, function (p) {
+		console.log("Pushed", p._id);
+		// if pod.outstandingPushes == 0, then go on...?
+	});
+}
+
+// running this repeatedly results in duplicated contacts...
+for (var i in contacts) {
+	var contact = contacts[i];
+	pod = connection[contact.from._id];
+	var contact = {isContact:true,
+				   siteURL:contact.to._id}
+	pod.push(contact, 
+			 function(c) {console.log('pushed connection', c);});
+};
